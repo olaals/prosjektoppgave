2f3cbf1c721fe09d379cb3e0455a1d4d0ed6d852
olaals <olaalstad96@gmail.com>
2020-10-06 22:08:41 +0200

    Update create patterns script

--- structured_light/dragly_testing/create_patterns.py
+++ structured_light/dragly_testing/create_patterns.py
@@ -1,5 +1,27 @@
 from pylab import zeros, ones, indices, uint8
 from PIL import Image
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+def show_image(img):
+    plt.imshow(img, cmap="gray")
+    plt.show()
+
+def get_argument_or_default(arg, default):
+    args = sys.argv
+    for i in range(len(args)):
+        if args[i] == arg:
+            return args[i+1]
+    print("Could not find argument for ", arg)
+    return default
+
+
+
+
+"""
 rows = 1024
 patterns = 7
 data = zeros((patterns, rows, rows))
@@ -8,5 +30,45 @@ for i in range(patterns):
     step = rows / divisions
     period = 2 * step
     data[i] = ones((rows, rows)) * ((indices((rows, rows))[1] % period) > step)
+    print(indices((rows, rows)).shape)
     img = Image.fromarray(255 * data[i].astype(uint8), mode='L')
-    img.save("patterns/pattern_{}.png".format(i + 1))
+    #img.save("patterns/pattern_{}.png".format(i + 1))
+
+"""
+
+def create_pattern_img(patterns, rows):
+    img = np.zeros((rows, rows))
+    divisions = 2 ** (patterns+1)
+    step = rows / divisions
+    period = 2 * step
+    img = ones((rows, rows)) * ((indices((rows, rows))[1] % period) > step) * 255
+    #img[:,:] = (np.array(list(range(ROWS))).astype(np.uint8)%period > step)*255
+    return img
+
+def create_inverse_pattern_img(patterns, rows):
+    img = np.zeros((rows, rows))
+    divisions = 2 ** (patterns+1)
+    step = rows / divisions
+    period = 2 * step
+    img = ones((rows, rows)) * ((indices((rows, rows))[1] % period) < step) * 255
+    return img
+
+
+if __name__ == '__main__':
+    ROWS = 1024
+    PATTERNS = 7
+    SAVE_FOLDER = os.path.join(os.getcwd(), get_argument_or_default("--output", "patterns"))
+    PATTERN_FILE_NAME = "pattern_inv"
+    EXTENSION = ".png"
+
+    ind = 0
+    for i in range(PATTERNS):
+        pattern_img = create_inverse_pattern_img(i, ROWS)
+        filepath = os.path.join(SAVE_FOLDER, PATTERN_FILE_NAME + "-" +str(ind) + EXTENSION)
+        cv2.imwrite(filepath, pattern_img)
+        ind += 1
+
+
+
+
+
